cca7984b5606a3a69f1990362dd3b4f80dac591c
joacoJawer <joacojota@gmail.com>
2024-07-01 16:07:01 -0300

    boludeces

--- marketplace/src/main/java/com/example/shoppear/marketplace/controllers/auth/AuthenticationController.java
+++ marketplace/src/main/java/com/example/shoppear/marketplace/controllers/auth/AuthenticationController.java
@@ -1,6 +1,7 @@
 package com.example.shoppear.marketplace.controllers.auth;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,12 +19,14 @@ public class AuthenticationController {
 
     private final AuthenticationService service;
 
+    @CrossOrigin (origins = "http://localhost:5173")
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody NuevoUsuarioRequest request) {
         return ResponseEntity.ok(service.register(request));
     }
 
+    @CrossOrigin (origins = "http://localhost:5173")
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request) {
